ffb5d8e9488a94db003652c9faff60a4e8fc54d3
prasanth <prashanthmsktm@gmail.com>
2019-12-31 23:34:35 +0530

    dbscript merge issue resolved

--- Aurizon-backend/db_scripts/script.sql
+++ Aurizon-backend/db_scripts/script.sql
@@ -142,7 +142,7 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `password` varchar(250) NOT NULL,
-  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_date` datetime NULL DEFAULT CURRENT_TIMESTAMP,
   `isActive` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
